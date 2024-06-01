7588597a639f67ca1bd94fc31ae6dfde2ff6f950
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-06-01 23:40:12 +0600

    installed and conneted mongodb to the server

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
   origin: ["http://localhost:5173"],
-  credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
 }));
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.mxltse8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const planetCollection = client.db("sample_guides").collection("planets");
+
+    app.get("/planets", async (req, res)  => {
+      const result = await planetCollection.find().toArray();
+      res.send(result);
+    } )
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
